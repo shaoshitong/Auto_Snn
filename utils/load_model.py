b26397a09ab01dce413c7204e2f39b3162e8b495
shaoshitong <1090784053@qq.com>
2021-11-30 23:30:10 +0800

    add load_model

---
+++ utils/load_model.py
@@ -0,0 +1,12 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+USE_FUNCTION={}
+def load_moad(path,model):
+    model_dict=torch.load(path)["snn_state_dict"]
+    model_state_dict=model.state_dict()
+    pretrained_dict = {k: v for k, v in model_dict.items() if k in model_state_dict and v.shape == model_state_dict[k].shape}
+    model_state_dict.update(pretrained_dict)
+    model.load_state_dict(model_state_dict)
+    return model
